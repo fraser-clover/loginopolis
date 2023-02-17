c4c6a804acab5f27ef0f66b7ded8e830119f0032
Fraser Clover <leesel.fraser@clover.com>
2023-02-17 14:37:44 -0500

    pass 3 tests

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 const { User } = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const hashPassword = async (password, saltCount) => {
+    const hash = await bcrypt.hash(password, saltCount);
+    return hash;
+}
+
 
 app.get('/', async (req, res, next) => {
   try {
@@ -16,9 +23,46 @@ app.get('/', async (req, res, next) => {
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post('/register', async (req, res, next) => {
+   try {
+        const { username, password } = req.body;
+        
+        const hashedPw = await hashPassword(password, 9);
+        let {id} = await User.create({username: username, password: hashedPw});
+        res.send({ message: "success", username})
+        
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+
+app.post('/login', async (req, res, next) => {
+  try {
+
+      const { username, password } = req.body;
+      const [foundUser] = await User.findAll({where: {username}});
+
+      if(!foundUser) {
+        res.sendStatus(401)
+      } else {
+        const isMatch = await bcrypt.compare(password, foundUser.password);
+        if(isMatch){
+          res.send({message: 'success', username})
+        } else {
+          res.sendStatus(401)
+        }
+       
+      }
+      
+  } catch(error) {
+      console.log(error);
+  }
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
